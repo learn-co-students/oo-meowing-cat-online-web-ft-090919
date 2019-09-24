e189fd26bb16a5133407c3639dedb81d50c6aa09
Richard Pascarelli <rpavesi84@gmail.com>
2019-09-24 01:31:02 +0000

    Done.

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,25 +1,6 @@
-#class Cat 
-#
-#  def cat=(cat_name) 
-#  @name=cat_name
-#  end
-
-#  def name
-#    @name
-#  end
-  
-#  def meow
-#    puts "meow!"
-    
-#end
-
-
 class Cat
-  
-  attr_accessor :name
-  
+ attr_accessor :name
   def meow
     puts "meow!"
   end
-
 end
